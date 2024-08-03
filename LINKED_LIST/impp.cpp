be9a09b004f434723df0b6952cb12960454aa31d
PallaviCodeVoyage <pallaviol0931@gmail.com>
2024-08-03 16:04:17 +0530

    Create impp.cpp

---
+++ LINKED_LIST/impp.cpp
@@ -0,0 +1,21 @@
+while(cur)
+        {
+            nextnode=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=nextnode;
+            
+        }//way 1 of reversing ll
+//eg:->5->6->7
+
+//way 2
+ListNode* reverseLL(ListNode* node) {
+        if(node == NULL || node->next == NULL)
+            return node;
+        ListNode* last = reverseLL(node->next);
+        node->next->next = node;
+        node->next = NULL;
+        return last;
+    }
+
+//making  function and returning the pointer of the last node whic will now be hed imagination sir
