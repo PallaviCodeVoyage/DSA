36814ff63d37e6ebe8989fa08a2adc079479be95
PallaviCodeVoyage <pallaviol0931@gmail.com>
2024-09-08 18:37:15 +0530

    Update 646. Maximum Length of Pair Chain.cpp

--- DP/646. Maximum Length of Pair Chain.cpp
+++ DP/646. Maximum Length of Pair Chain.cpp
@@ -59,3 +59,31 @@ public:
     }
 };
 /*o(nlogn)*/
+
+
+
+//Approach-2 (Using Simple LIS Bottom Up) - Since Qn asks to take pairs in any order, we sort it in the beginning
+class Solution {
+public:
+    
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int n = pairs.size();
+        sort(begin(pairs), end(pairs));
+        
+        vector<int> t(n, 1);
+        int maxL = 1;
+        
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<i; j++) {
+                
+                if(pairs[j][1] < pairs[i][0]) {
+                    t[i] = max(t[i], t[j]+1);
+                    maxL = max(maxL, t[i]);
+                }
+            }
+        }
+        
+        return maxL;
+    }
+};
+
