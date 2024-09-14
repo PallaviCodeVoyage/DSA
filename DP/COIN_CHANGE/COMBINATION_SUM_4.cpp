26cd6b07ec3d30ee0ceafbd296400db2d13fb7b3
PallaviCodeVoyage <pallaviol0931@gmail.com>
2024-09-14 10:57:19 +0530

    Create COMBINATION_SUM_4.cpp

---
+++ DP/COIN_CHANGE/COMBINATION_SUM_4.cpp
@@ -0,0 +1,23 @@
+//same as combination finding in coin change but the condition 1,2 is same as 2,1
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        // Use long long for dp to avoid overflow issues
+        vector<long long> dp(target + 1, 0);
+        // Base case: there's exactly 1 way to make target 0 (by not picking any numbers)
+        dp[0] = 1;
+        
+        // Iterate through all possible target amounts from 1 to the given target
+        for (int i = 1; i <= target; i++) {
+            // For each number in the nums array, see if it can contribute to the current target i
+            for (int num : nums) {
+                if (i - num >= 0) {
+                    dp[i] += dp[i - num];  // Add the number of ways to make (i - num) to dp[i]
+                }
+            }
+        }
+
+        // dp[target] will contain the number of ways to form the target
+        return dp[target];
+    }
+};
