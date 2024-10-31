2041b5f23083cb010499fd02ffee1f15d8cac021
PallaviCodeVoyage <pallaviol0931@gmail.com>
2024-10-31 06:57:03 +0530

    Create 116. Populating Next Right Pointers in Each Node.cpp

---
+++ LINKED_LIST/116. Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==nullptr)
+        {
+            return root;
+        }
+        //trying to apply bfs in trees(strong bfs)
+        queue<Node*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int s=q.size();
+            for(int i=0;i<s;i++)
+            {
+                Node*node=q.front();
+                q.pop();
+                if(i<s-1)
+                {
+                    node->next=q.front();
+                }
+                if(node->left)
+                {
+                    q.push(node->left);
+                }
+                if(node->right)
+                {
+                    q.push(node->right);
+                }
+
+            }
+
+
+        }
+                  return root;
+        
+    }
+};
